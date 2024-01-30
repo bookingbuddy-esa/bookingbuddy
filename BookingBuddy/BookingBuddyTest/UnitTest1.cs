296ea0351b2308a4478d163783650e4ed6a9f33a
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-01-30 13:05:42 +0000

    Adicionar projeto vazio de testes

---
+++ BookingBuddy/BookingBuddyTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BookingBuddyTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
