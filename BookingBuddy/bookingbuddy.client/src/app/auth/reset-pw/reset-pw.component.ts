ed24459c07338c76455e0b2fce7e0a1d6cfdf7ac
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-02-01 23:29:28 +0000

    Adicionar página de reset da palavra-passe

--- BookingBuddy/bookingbuddy.client/src/app/auth/reset-pw/reset-pw.component.ts
+++ BookingBuddy/bookingbuddy.client/src/app/auth/reset-pw/reset-pw.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { AuthorizeService } from "../authorize.service";
+import { ActivatedRoute } from '@angular/router';
+import { query } from '@angular/animations';
 
 @Component({
   selector: 'app-reset-pw',
@@ -11,20 +13,72 @@ export class ResetPwComponent {
   errors: string[] = [];
   resetPWForm!: FormGroup;
   emailSent: boolean = false;
+  token: string = "";
+  uid: string = "";
+  validUrl: boolean = false;
 
-  constructor(private authService: AuthorizeService, private formBuilder: FormBuilder) {
-
-  }
+  constructor(private authService: AuthorizeService, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
     this.errors = [];
     this.resetPWForm = this.formBuilder.group(
       {
-        email: ['', [Validators.required, Validators.email]],
-
+        password: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]]
       });
+
+    // implementação sem o Router
+    var url = location.href;
+    var query = url.split('?')[1];
+    if (query !== undefined) {
+      query.split('&').forEach(param => {
+        var elem = param.split('=');
+        if (elem[0] === "uid") {
+          this.uid = decodeURIComponent(elem[1]);
+        }
+        if (elem[0] === "token") {
+          this.token = decodeURIComponent(elem[1]);
+        }
+      });
+    }
+    if (this.uid !== "" && this.token !== "") {
+      this.validUrl = true
+    }
   }
+
   public reset(_: any) {
-    
+    if (!this.resetPWForm.valid) {
+      return;
+    }
+    this.errors = [];
+    const password = this.resetPWForm.get('password')?.value;
+    const confirmPassword = this.resetPWForm.get('confirmPassword')?.value;
+    if (password !== confirmPassword) {
+      this.errors.push('Passwords do not match.');
+      return;
+    }
+    this.authService.resetPassword(this.uid, this.token, password).forEach(
+      response => {
+        if (response) {
+          location.href = "/signin"
+        }
+      }).catch(
+        error => {
+          if (error.error) {
+            const errorObj = JSON.parse(error.error);
+            if (errorObj && errorObj.errors) {
+              // problem details { "field1": [ "error1", "error2" ], "field2": [ "error1", "error2" ]}
+              const errorList = errorObj.errors;
+              for (let field in errorList) {
+                if (Object.hasOwn(errorList, field)) {
+                  let list: string[] = errorList[field];
+                  for (let idx = 0; idx < list.length; idx += 1) {
+                    this.errors.push(`${field}: ${list[idx]}`);
+                  }
+                }
+              }
+            }
+          }
+        });
   }
 }
