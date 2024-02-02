2bef819cfee8f69c5b9aa3a270b26fa9199b3132
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-02-02 01:40:30 +0000

    Adicionar à homepage de testes o utilizador atual e a opção de logout

--- BookingBuddy/bookingbuddy.client/src/app/homepage/homepage.component.ts
+++ BookingBuddy/bookingbuddy.client/src/app/homepage/homepage.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthorizeService } from '../auth/authorize.service';
+import { UserInfo } from '../auth/authorize.dto';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
-export class HomepageComponent {
-
+export class HomepageComponent implements OnInit {
+  signedIn: boolean = false;
+  user: UserInfo | undefined;
+  constructor(private authService: AuthorizeService, private router: Router) {
+    this.authService.isSignedIn().forEach(
+      isSignedIn => {
+        this.signedIn = isSignedIn;
+        if (isSignedIn) {
+          this.authService.user().forEach(user => this.user = user);
+        }
+      });
+  }
+  ngOnInit(): void {
+    this.authService.onStateChanged().forEach(isSignedIn => {
+      this.signedIn = isSignedIn;
+      if (isSignedIn) {
+        this.authService.user().forEach(user => this.user = user);
+      }
+    });
+  }
+  public logout() {
+    this.authService.signOut().forEach(response => {
+      if (response) {
+        this.router.navigateByUrl('');
+      }
+    });
+  }
 }
