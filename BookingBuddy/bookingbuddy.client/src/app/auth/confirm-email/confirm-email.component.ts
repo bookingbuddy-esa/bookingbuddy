eb0020e2da008749bb8e30e5f74a827b00dc9122
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-02-03 04:01:33 +0000

    Adicionar página de confirmação de email

---
+++ BookingBuddy/bookingbuddy.client/src/app/auth/confirm-email/confirm-email.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthorizeService } from '../authorize.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-confirm-email',
+  templateUrl: './confirm-email.component.html',
+  styleUrl: './confirm-email.component.css'
+})
+export class ConfirmEmailComponent implements OnInit {
+  submitting: boolean = false;
+  errors: string[] = [];
+
+  constructor(private authService: AuthorizeService, private route: ActivatedRoute, private router: Router) { }
+
+  ngOnInit(): void {
+    this.submitting = true;
+    var uid = this.route.snapshot.queryParamMap.get("uid");
+    var token = this.route.snapshot.queryParamMap.get("token");
+    if (uid !== null && token !== null) {
+      this.authService.confirmEmail(uid, token).forEach(
+        response => {
+          if (response) {
+            this.submitting = false;
+          }
+        }).catch(
+          error => {
+            if (error.error) {
+              const errorObj = JSON.parse(error.error);
+              Object.entries(errorObj).forEach((entry) => {
+                const [key, value] = entry;
+                var code = (value as any)["code"]
+                var description = (value as any)["description"];
+                if (code === "InvalidToken" || code === "UserNotFound") {
+                  //em caso de o token ou o user serem inválidos
+                  //this.validUrl = false;
+                }
+                this.errors.push(description);
+              });
+            }
+            this.submitting = false;
+          });
+    }
+  }
+}
