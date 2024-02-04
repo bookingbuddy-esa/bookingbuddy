a17e6211ff32f8246330605f7fb6dbc9ad502115
Eduardo Ervideira <ejcept@gmail.com>
2024-02-04 17:27:41 +0000

    Adicionar verificação client-side Adicionar lazy-loading após registo

--- BookingBuddy/bookingbuddy.client/src/app/auth/signin/signin.component.ts
+++ BookingBuddy/bookingbuddy.client/src/app/auth/signin/signin.component.ts
@@ -70,7 +70,7 @@ export class SignInComponent implements OnInit {
     this.errors = [];
 
     const userName = this.signinForm.get('email')?.value;
-    const password = this.signinForm.get('password')?.value;
+    const password = this.signinForm.get('password')?.value
 
     this.authService.signIn(userName, password).forEach(
       response => {
