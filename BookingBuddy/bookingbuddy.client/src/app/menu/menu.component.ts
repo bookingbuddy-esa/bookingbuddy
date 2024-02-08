4298e21f348b2ca58ea0dccf2f108c508dace072
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-02-08 23:17:41 +0000

    BB0-536 Adicionar login e registo à página principal

--- BookingBuddy/bookingbuddy.client/src/app/menu/menu.component.ts
+++ BookingBuddy/bookingbuddy.client/src/app/menu/menu.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AuthorizeService} from "../auth/authorize.service";
+import {Router} from "@angular/router";
+import {UserInfo} from "../auth/authorize.dto";
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
+  protected signedIn: boolean = false;
+  protected user: UserInfo | undefined;
+
+  constructor(private authService: AuthorizeService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.authService.isSignedIn().forEach(
+      isSignedIn => {
+        this.signedIn = isSignedIn;
+        if (isSignedIn) {
+          this.authService.user().forEach(user => this.user = user);
+        }
+      });
+    this.authService.onStateChanged().forEach(isSignedIn => {
+      this.signedIn = isSignedIn;
+      if (isSignedIn) {
+        this.authService.user().forEach(user => this.user = user);
+      }
+    });
+  }
+
   isExpanded = false;
 
   collapse() {
@@ -14,5 +39,9 @@ export class MenuComponent {
 
   toggle() {
     this.isExpanded = !this.isExpanded;
+  }
+
+  public signOut() {
+    this.authService.signOut().subscribe(() => this.router.navigate(['']));
   }
 }
