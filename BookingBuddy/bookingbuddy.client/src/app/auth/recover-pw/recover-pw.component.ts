ff7c902c60e99f8ec3a31badbc074a5e1f604561
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-02-09 23:19:58 +0000

    Atualizar página de recuperação da palavra-passe

--- BookingBuddy/bookingbuddy.client/src/app/auth/recover-pw/recover-pw.component.ts
+++ BookingBuddy/bookingbuddy.client/src/app/auth/recover-pw/recover-pw.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { AuthorizeService } from "../authorize.service";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthorizeService} from "../authorize.service";
 
 @Component(
   {
@@ -13,6 +13,7 @@ export class RecoverPwComponent implements OnInit {
   errors: string[] = [];
   recoverPWForm!: FormGroup;
   emailSent: boolean = false;
+  submitting: boolean = false;
 
   constructor(private authService: AuthorizeService, private formBuilder: FormBuilder) {
 
@@ -26,8 +27,15 @@ export class RecoverPwComponent implements OnInit {
 
       });
   }
+
+  get emailFormField() {
+    return this.recoverPWForm.get('email');
+  }
+
   public recover(_: any) {
+    this.submitting = true;
     if (!this.recoverPWForm.valid) {
+      this.submitting = false;
       return;
     }
 
@@ -38,25 +46,27 @@ export class RecoverPwComponent implements OnInit {
     this.authService.recoverPassword(email).forEach(
       response => {
         if (response) {
+          this.submitting = false;
           this.emailSent = true;
         }
       }).catch(
-        error => {
-          if (error.error) {
-            const errorObj = JSON.parse(error.error);
-            if (errorObj && errorObj.errors) {
-              const errorList = errorObj.errors;
-              for (let field in errorList) {
-                if (Object.prototype.hasOwnProperty.call(errorList, field)) {
-                  let list: string[] = errorList[field];
-                  for (let idx = 0; idx < list.length; idx += 1) {
-                    this.errors.push(`${field}: ${list[idx]}`);
-                  }
+      error => {
+        if (error.error) {
+          const errorObj = JSON.parse(error.error);
+          if (errorObj && errorObj.errors) {
+            const errorList = errorObj.errors;
+            for (let field in errorList) {
+              if (Object.prototype.hasOwnProperty.call(errorList, field)) {
+                let list: string[] = errorList[field];
+                for (let idx = 0; idx < list.length; idx += 1) {
+                  this.errors.push(`${field}: ${list[idx]}`);
                 }
               }
             }
           }
         }
-      );
+        this.submitting = false;
+      }
+    );
   }
 }
