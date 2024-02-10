f970a8986128b203c7f1be8f6597a761d14a6578
Eduardo Ervideira <ejcept@gmail.com>
2024-02-10 20:29:19 +0000

    Continuar implementação página de visualização de anúncio

--- BookingBuddy/bookingbuddy.client/src/app/property-ad/property-ad-create/property-ad-create.component.ts
+++ BookingBuddy/bookingbuddy.client/src/app/property-ad/property-ad-create/property-ad-create.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from '@angular/router';
 
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, Subject, catchError, map, of } from 'rxjs';
+
 @Component({
   selector: 'app-property-ad-create',
   templateUrl: './property-ad-create.component.html',
@@ -11,18 +15,29 @@ export class PropertyAdCreateComponent {
   errors: string[] = [];
   createPropertyAdForm!: FormGroup;
 
-
   /**
    * Construtor da classe SignInComponent.
    * @param formBuilder Construtor de formulários do Angular.
    * @param router Router do Angular.
    */
-  constructor( private formBuilder: FormBuilder, private router: Router) {
+  constructor(private http: HttpClient, private formBuilder: FormBuilder, private router: Router) {
     
   }
-  
+  ngOnInit(): void {
+    this.errors = [];
+    this.createPropertyAdForm = this.formBuilder.group(
+      {
+        // campos do form aqui
+        name: ['', [Validators.required]],
+        location: ['', [Validators.required]],
+        pricePerNight: ['', [Validators.required]],
+        amenityIds: [''],
+        imagesUrl: ['']
+      });
+  }
 
   public create(_: any) {
-  
+    console.log(this.createPropertyAdForm.value)
+    //return this.http.post<any>('/api/properties/create', this.createPropertyAdForm.value)
   }
 }
--- BookingBuddy/bookingbuddy.client/src/app/property-ad/property-ad-retrieve/property-ad-retrieve.component.ts
+++ BookingBuddy/bookingbuddy.client/src/app/property-ad/property-ad-retrieve/property-ad-retrieve.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Property } from '../../models/property';
+
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-property-ad-retrieve',
   templateUrl: './property-ad-retrieve.component.html',
   styleUrl: './property-ad-retrieve.component.css'
 })
-export class PropertyAdRetrieveComponent {
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class PropertyAdRetrieveComponent implements OnInit {
+  // get the property ad id from the route
+  // retrieve the property ad from the server
+  // display the property ad
+  // handle the case when the property ad is not found
+  // handle the case when the server is not available
+
+  property: Property | undefined;
+  id: number = 0;
+
+  constructor(/*private service: PeopleService,*/ private route: ActivatedRoute) {
+  }
+
+  ngOnInit() {
+    this.id = this.route.snapshot.params['id'];
+    this.property = { propertyId: "1", landlordId: "1", name: "Casa do Forte", location: "Peniche, Portugal", pricePerNight: 100, imagesUrl: [""] };
+  }
 
 }
