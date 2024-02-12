02bc1b447cd4109bcaa6f68cd217b8e1864f8fae
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-02-12 01:19:33 +0000

    Atualizar ambiente para distinguir dev de prod

--- BookingBuddy/bookingbuddy.client/src/app/auth/external-providers/microsoft/microsoft.component.ts
+++ BookingBuddy/bookingbuddy.client/src/app/auth/external-providers/microsoft/microsoft.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component} from '@angular/core';
+import {environment} from '../../../../environments/environment';
 
 @Component({
   selector: 'app-microsoft',
@@ -8,15 +8,14 @@ import { Router } from '@angular/router';
 })
 export class MicrosoftComponent {
   public microsoftLogin() {
-    var tenantId = "46464aab-10be-4730-96d1-7f0d52c6fefe";
-    var clientId = "def42587-7c39-4a86-8114-2d22b5362517";
-    var responseType = "id_token";
-    var redirectUri = "https://localhost:7213/api/microsoft";
-    var scope = "openid email profile user.read";
-    var response_mode = "form_post";
-    var nonce = "something-random";
-    var loginLink = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}&scope=${scope}&response_mode=${response_mode}&nonce=${nonce}`
-    var encodedLink = encodeURI(loginLink);
-    document.location.href = encodedLink;
+    const tenantId = environment.microsoftTenantId;
+    const clientId = environment.microsoftClientId;
+    const responseType = "id_token";
+    const redirectUri = `${environment.apiUrl}/api/microsoft`;
+    const scope = "openid email profile user.read";
+    const response_mode = "form_post";
+    const nonce = "something-random";
+    const loginLink = `https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/authorize?client_id=${clientId}&response_type=${responseType}&redirect_uri=${redirectUri}&scope=${scope}&response_mode=${response_mode}&nonce=${nonce}`;
+    document.location.href = encodeURI(loginLink);
   }
 }
