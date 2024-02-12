7b656590d73f98200a982e045f19d091cf52831a
Pedro Anjos <101123927+p3dro4@users.noreply.github.com>
2024-02-12 02:48:22 +0000

    Atualizar ambiente para distinguir dev de prod (#14)

---
+++ BookingBuddy/bookingbuddy.client/src/environments/environment.development.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  apiUrl: 'https://localhost:7213',
+  googleClientId: '780818883221-79bvdf437f1rkq51jjvdl1l3n62pp5nv.apps.googleusercontent.com',
+  microsoftClientId: 'def42587-7c39-4a86-8114-2d22b5362517',
+  microsoftTenantId: '46464aab-10be-4730-96d1-7f0d52c6fefe'
+};
