e5915183f661fabe04d0d22e007c5a8b601c462f
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-02-13 20:07:41 +0000

    Adicionar utilizadores padrão à base de dados

--- BookingBuddy/BookingBuddy.Server/Data/BookingBuddyServerContext.cs
+++ BookingBuddy/BookingBuddy.Server/Data/BookingBuddyServerContext.cs
@@ -9,19 +9,13 @@ namespace BookingBuddy.Server.Data
     /// Classe que representa o contexto da base de dados da aplicação.
     /// </summary>
     /// <param name="options">As opções para a criação do contexto</param>
-    public class BookingBuddyServerContext(DbContextOptions<BookingBuddyServerContext> options) : IdentityDbContext<ApplicationUser>(options)
+    public class BookingBuddyServerContext(DbContextOptions<BookingBuddyServerContext> options)
+        : IdentityDbContext<ApplicationUser>(options)
     {
         /// <summary>
         /// Propriedade que diz respeito à comodidade da propriedade física.
         /// </summary>
         public DbSet<Amenity> PropertyAmenity { get; set; } = default!;
-
-        /*
-        /// <summary>
-        /// Propriedade que diz respeito ao proprietário da propriedade física.
-        /// </summary>
-        public DbSet<Landlord> Landlord { get; set; } = default!;
-        */
 
         /// <summary>
         /// Propriedade que diz respeito à propriedade física.
@@ -29,5 +23,105 @@ namespace BookingBuddy.Server.Data
         public DbSet<Property> Property { get; set; } = default!;
 
         public DbSet<BlockedDate> BlockedDate { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var adminRole = new IdentityRole
+            {
+                Name = "admin",
+                NormalizedName = "ADMIN",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            };
+            var userRole = new IdentityRole
+            {
+                Name = "user",
+                NormalizedName = "USER",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            };
+            var landlordRole = new IdentityRole
+            {
+                Name = "landlord",
+                NormalizedName = "LANDLORD",
+                ConcurrencyStamp = Guid.NewGuid().ToString()
+            };
+
+            builder.Entity<IdentityRole>().HasData(
+                adminRole,
+                userRole,
+                landlordRole
+            );
+
+            var adminUser = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "admin",
+                UserName = "bookingbuddy.admin@bookingbuddy.com",
+                NormalizedUserName = "BOOKINGBUDDY.ADMIN@BOOKINGBUDDY.COM",
+                Email = "bookingbuddy.admin@bookingbuddy.com",
+                NormalizedEmail = "BOOKINGBUDDY.ADMIN@BOOKINGBUDDY.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+            };
+            adminUser.PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(adminUser, "adminBB123!");
+
+            var userUser = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "user",
+                UserName = "bookingbuddy.user@bookingbuddy.com",
+                NormalizedUserName = "BOOKINGBUDDY.USER@BOOKINGBUDDY.COM",
+                Email = "bookingbuddy.user@bookingbuddy.com",
+                NormalizedEmail = "BOOKINGBUDDY.USER@BOOKINGBUDDY.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+            };
+            userUser.PasswordHash = new PasswordHasher<ApplicationUser>().HashPassword(userUser, "userBB123!");
+
+            var landlordUser = new ApplicationUser
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = "landlord",
+                UserName = "bookingbuddy.landlord@bookingbuddy.com",
+                NormalizedUserName = "BOOKINGBUDDY.LANDLORD@BOOKINGBUDDY.COM",
+                Email = "bookingbuddy.landlord@bookingbuddy.com",
+                NormalizedEmail = "BOOKINGBUDDY.LANDLORD@BOOKINGBUDDY.COM",
+                EmailConfirmed = true,
+                LockoutEnabled = false,
+                SecurityStamp = Guid.NewGuid().ToString(),
+                ConcurrencyStamp = Guid.NewGuid().ToString(),
+            };
+            landlordUser.PasswordHash =
+                new PasswordHasher<ApplicationUser>().HashPassword(landlordUser, "landlordBB123!");
+
+            builder.Entity<ApplicationUser>().HasData(
+                adminUser,
+                userUser,
+                landlordUser
+            );
+
+            builder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string>
+                {
+                    RoleId = adminRole.Id,
+                    UserId = adminUser.Id
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = userRole.Id,
+                    UserId = userUser.Id
+                },
+                new IdentityUserRole<string>
+                {
+                    RoleId = landlordRole.Id,
+                    UserId = landlordUser.Id
+                }
+            );
+        }
     }
 }
