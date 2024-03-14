68a348b7be2edc15517a4bacafe75ee876923536
p3dro4 <101123927+p3dro4@users.noreply.github.com>
2024-03-14 23:22:50 +0000

    BB0-562: Adicionar projeto de testes (Katalon) para testagem do cliente Adicionar os casos de teste para a autenticação - Testar o percurso de login - Testar o percurso de registo - Testar o percurso de recuperar palavra-passe

---
+++ BookingBuddy/BookingBuddyClientTest/Object Repository/Authentication/Page_RecoverPassword/div_recoverSuccess.rs
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_recoverSuccess</name>
+   <tag></tag>
+   <elementGuidId>6548b41c-2d2c-4001-aa22-7d06080b4374</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Iniciar Sessão'])[1]/following::div[3]</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>div.d-flex.w-50.my-5.flex-column.align-items-center.text-center</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorCollection>
+      <entry>
+         <key>SMART_LOCATOR</key>
+         <value>internal:text=&quot;Um email foi enviado com instruções sobre como redefinir a sua palavra passe.Con&quot;i</value>
+      </entry>
+   </smartLocatorCollection>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+      <webElementGuid>2d860098-bbc3-4349-aff5-f682f1704d19</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>d-flex w-50 my-5 flex-column align-items-center text-center</value>
+      <webElementGuid>936b387c-4db2-44e2-8528-def3f11c3900</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Um email foi enviado com instruções sobre como redefinir a sua palavra passe.Confira a sua caixa de entrada.</value>
+      <webElementGuid>262e336c-c129-4df8-9200-7e68502dd219</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/app-root[1]/main[1]/div[@class=&quot;container-fluid d-flex flex-grow-1&quot;]/app-recover-pw[1]/div[@class=&quot;d-flex w-100 flex-column align-items-center justify-content-between&quot;]/div[@class=&quot;d-flex w-50 my-5 flex-column align-items-center text-center&quot;]</value>
+      <webElementGuid>d1e86e50-751f-4fe1-84bb-b5370941d10e</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Iniciar Sessão'])[1]/following::div[3]</value>
+      <webElementGuid>6648925b-bacb-4de2-9103-2fb6d224b591</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Registar'])[1]/following::div[3]</value>
+      <webElementGuid>fbaf114d-97e1-4ff9-8b8f-5105d74b0039</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Clique aqui'])[1]/preceding::div[1]</value>
+      <webElementGuid>73067975-40a3-4d70-b15f-e48f86dede76</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//app-recover-pw/div/div</value>
+      <webElementGuid>570b8792-1198-495a-8f9b-2c6f2789b29c</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//div[(text() = 'Um email foi enviado com instruções sobre como redefinir a sua palavra passe.Confira a sua caixa de entrada.' or . = 'Um email foi enviado com instruções sobre como redefinir a sua palavra passe.Confira a sua caixa de entrada.')]</value>
+      <webElementGuid>420b135f-7707-41b9-9ff9-34f2aca770c0</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
