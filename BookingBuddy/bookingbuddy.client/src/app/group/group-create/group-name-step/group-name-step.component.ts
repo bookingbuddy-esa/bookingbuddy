f0c9c55668098482ff3811a315a39d2566a45062
andremeseiro <202100225@estudantes.ips.pt>
2024-03-14 21:55:37 +0000

    Componente group-name-step terminado

--- BookingBuddy/bookingbuddy.client/src/app/group/group-create/group-name-step/group-name-step.component.ts
+++ BookingBuddy/bookingbuddy.client/src/app/group/group-create/group-name-step/group-name-step.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-group-name-step',
@@ -6,7 +7,37 @@ import { Component } from '@angular/core';
   styleUrl: './group-name-step.component.css'
 })
 export class GroupNameStepComponent {
+  @Input() errors: string[] = [];
+  @Input() groupName: GroupName | undefined;
+  @Output() groupNameValid = new EventEmitter<boolean>();
+  @Output() groupNameSubmit = new EventEmitter<GroupName>();
+  protected currentGroupName: GroupName | undefined;
+  protected nameForm = this.formBuilder.group({
+    name: ['', Validators.required, Validators.minLength(3), Validators.maxLength(20)]
+  });
 
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  ngOnInit(): void {
+    if (this.groupName) {
+      this.currentGroupName = this.groupName;
+      this.nameForm.get('name')?.setValue(this.groupName.name);
+    }
+
+    this.nameForm.valueChanges.forEach(() => {
+      this.groupNameValid.emit(this.nameForm.valid);
+      this.currentGroupName = {
+        name: this.nameFormField!.value ?? ""
+      };
+      this.groupNameSubmit.emit(this.currentGroupName);
+    })
+  }
+
+  get nameFormField() {
+    return this.nameForm.get('name');
+  }
 }
 
 export interface GroupName {
